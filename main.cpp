27aff7479402fe955efb856b8bd82948747f6f8b
strale15 <karte32114@gmail.com>
2024-01-14 22:57:10 +0100

    Deleted test cube

--- main.cpp
+++ main.cpp
@@ -146,15 +146,8 @@ static void DrawHud(Shader& hudShader, unsigned hudTex) {
     glBindTexture(GL_TEXTURE_2D, 0);
 }
 
-static void DrawScene(Shader& shader, Params &params, bool isBack = false) {
+static void DrawScene(Shader& shader, Params &params) {
     glm::mat4 m = glm::mat4(1.0f);
-
-    //test
-    m = glm::translate(glm::mat4(1.0), glm::vec3(-10.0, 0.0, 0.0));
-    m = glm::rotate(m, glm::radians(180.f), glm::vec3(0.0, 1.0, 0.0));
-    m = glm::scale(m, glm::vec3(1.0, 1.0, 1.0));
-    shader.setMat4("uModel", m);
-    simpleCube->Render(&shader, 0, 1, 1);
 
     //Road
     m = glm::translate(glm::mat4(1.0), glm::vec3(0.0, -1.0, 0.0));
